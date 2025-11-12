29faa47e06825d1818b2198e8dc3496a27c44192
Kaue <kauesabino@hotmail.com>
2025-11-11 21:37:39 -0300

    feat: adicionando interface de livro e componente de listagem

--- organo/src/app/app.component.ts
+++ organo/src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CabecalhoComponent } from "../shared/components/cabecalho/cabecalho.component";
 import { RodapeComponent } from "../shared/components/rodape/rodape.component";
-import { LivroComponent } from "../shared/components/livro/livro.component";
+import { ListaLivrosComponent } from "../shared/components/lista-livros/lista-livros.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CabecalhoComponent, RodapeComponent, LivroComponent],
+  imports: [RouterOutlet, CabecalhoComponent, RodapeComponent, ListaLivrosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
